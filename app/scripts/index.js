c695de5c74c0f71cc405bdbd0d91624ce21c6d3a
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-02-13 14:48:14 -0500

    style and warnings fixed

---
+++ app/scripts/index.js
@@ -0,0 +1,20 @@
+
+var Backbone = require('backbone');
+var $ = require('jquery');
+var likes = require('./models/like.js');
+
+console.log("like", likes.LikeButton);
+
+var imaButton = new likes.LikeButton();
+
+
+
+$('.like-button').on('click', function(event){
+  event.preventDefault();
+  imaButton.like();
+  imaButton.toJSON();
+  $('.like-button').text(imaButton.get('likes') + imaButton.get('label'));
+  //console.log('imabutton.like()',imaButton.like());
+  //console.log('clicked');
+  //console.log('imabutton.get', imaButton.get('likes'));
+});
---
+++ app/scripts/models/like.js
@@ -0,0 +1,33 @@
+var $ = require('jquery');
+var Backbone = require('backbone');
+
+
+
+var LikeButton = Backbone.Model.extend({
+  defaults: {
+    'likes': 0,
+    'label': ' like'
+  },
+  initialize: function() {
+    console.log('just for fun, initialize it');
+  },
+  like : function() {
+    var likings = this.get('likes');
+    this.set('likes', likings +1);
+    //console.log('i\'ve been liked');
+      // this.toJSON();
+  },
+  toJSON : function() {
+    if (this.get('likes') != 1) {
+      this.set('label', ' likes');
+    }
+    return Backbone.Model.prototype.toJSON.call(this);
+  }
+
+});
+
+
+
+module.exports = {
+  'LikeButton': LikeButton
+};
